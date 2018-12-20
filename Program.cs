d77746bc615035bd2f71a5f641f11cf7b596e68b
BilalyC <bilaly.camara@outlook.com>
2018-12-20 13:25:24 +0100

    Exercice 4 Complet

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exo4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int counter;
+            for (counter = 10; counter >= 0; counter--)
+            {
+                Console.WriteLine(counter);
+            }
+        }
+    }
+}
